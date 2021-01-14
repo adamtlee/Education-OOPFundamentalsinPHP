fb46789c9367f0b66c9cd8309b2efc6fd0911e6f
adamtlee <adam.lee346@gmail.com>
2021-01-14 14:44:41 -0700

    refactored sum

--- 02_Inheritance/Athlete.php
+++ 02_Inheritance/Athlete.php
@@ -11,9 +11,8 @@ class Collection
 
     public function sum($key)
     {
-        return array_sum(array_map(function($item) use ($key){
-            return $item->$key;
-        }, $this->items));
+        // return array_sum(array_map(fn($item) => $item->key, $this->items));
+        return array_sum(array_column($this->items, $key));
     }
 }
 
