b2191194e41920eee151e00417c69a64d8543029
adamtlee <adam.lee346@gmail.com>
2021-01-14 14:51:40 -0700

    overide example in awards

---
+++ 02_Inheritance/Award.php
@@ -0,0 +1,28 @@
+<?php
+
+class AwardType
+{
+    public function name()
+    {
+
+    }
+
+    public function difficulty()
+    {
+
+    }
+}
+
+class FirstFiveWins extends AwardType
+{
+    public function qualifyer($user)
+    {
+
+    }
+
+    // Override the parent behavior
+    public function name()
+    {
+
+    }
+}
